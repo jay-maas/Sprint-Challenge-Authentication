3446639f975d815ddd4fa38626e8eac83b2334d8
Jay Maas <jaymaas.dev@gmail.com>
2019-06-14 11:20:20 -0600

    Answers
    Provided self study answers in readme

--- config/routes.js
+++ config/routes.js
@@ -37,7 +37,7 @@ function login(req, res) {
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = generateToken(user);
         res.status(200).json({
-          token
+          token: token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
